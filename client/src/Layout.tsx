7aa757b84b11f8361429643411ba7a986672eda1
Denisssska <yarmoshkoden18m@gmail.com>
2023-11-30 20:11:43 +0300

    delete  react query

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -1,24 +1,19 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
 import { Outlet } from 'react-router-dom';
 
 import { Footer, Menu, Navbar, Preloader } from './components';
 
 import { ToasterProvider } from './providers/ToasterProvider';
-const queryClient = new QueryClient();
 
 const Layout = () => {
   return (
-    <div style={{position:'relative'}}  className="main">
+    <div style={{ position: 'relative' }} className="main">
       <Navbar />
       <div className="container">
         <div className="menuContainer">
           <Menu />
         </div>
         <div className="contentContainer">
-          <QueryClientProvider client={queryClient}>
-            <Outlet />
-          </QueryClientProvider>
+          <Outlet />
         </div>
       </div>
       <Footer />
