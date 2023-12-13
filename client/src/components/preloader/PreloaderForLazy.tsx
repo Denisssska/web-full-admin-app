885b63a3201da028bbdd5d3ba263d02c52f83a95
Denisssska <yarmoshkoden18m@gmail.com>
2023-12-13 19:03:32 +0300

    add style for preloader

--- client/src/components/preloader/PreloaderForLazy.tsx
+++ client/src/components/preloader/PreloaderForLazy.tsx
@@ -2,7 +2,7 @@ import './preloader.scss';
 
 export const PreloaderForLazy = () => {
   return (
-    <div className="preloader">
+    <div className="preloaderForLazy">
       <img src="/preloader.svg" alt="loading" />
     </div>
   );
